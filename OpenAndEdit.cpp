425560ac598003819e503acea421a036b2d0b1b8
Kyon Kun <mrkittenpuppy@gmail.com>
2022-06-16 00:31:05 -0400

    issue, trying to fix on line 244

--- OpenAndEdit.cpp
+++ OpenAndEdit.cpp
@@ -10,7 +10,7 @@
 #include <stdexcept>			// for exceptions
 #include "OpenAndEdit.h"
 
-#define VERSION_STAMP	"V 0.202"
+#define VERSION_STAMP	"V 0.205"
 
 namespace fs = std::filesystem; // possible to cutdown on the namespace extensions. using "fs" instead of "std::filesystme" works
 
@@ -21,7 +21,7 @@ short RenderShortFromStringTimes100(const std::string& message);
 short StringToShort(const std::string& message);
 int main() {
 
-	const std::string example_data =
+	std::string example_data =
 		"{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[-44.66,57.25],[-50.62,52.65],[-56.25,56.8],[-54,59.38],[-44.55,58.26],[-44.66,57.25]]]},\"properties\":{\"id\":0,\"height\":-109,\"biome\":12,\"type\":\"island\",\"population\":140,\"state\":2,\"province\":3,\"culture\":4,\"religion\":5,\"neighbors\":[1,7,6]}},{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[-40.16,51.19],[-42.53,52.2],[-44.21,54.22],[-44.66,57.25],[-44.55,58.26],[-44.1,58.6],[-41.17,58.26],[-36.56,52.65],[-38.14,51.53],[-40.16,51.19]]]},\"properties\":{\"id\":1,\"height\":-29,\"biome\":0,\"type\":\"ocean\",\"population\":0,\"state\":0,\"province\":0,\"culture\":0,\"religion\":0,\"neighbors\":[11,10,9,7,0,2,12]}},{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[-35.44,52.65],[-36.56,52.65],[-41.17,58.26],[-34.31,60.84],[-33.97,60.5],[-34.65,52.99],[-35.44,52.65]]]},\"properties\":{\"id\":2,\"height\":-70,\"biome\":0,\"type\":\"ocean\",\"population\":0,\"state\":0,\"province\":0,\"culture\":0,\"religion\":0,\"neighbors\":[13,12,1,3]}},{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[-30.6,51.76],[-34.65,52.99],[-33.97,60.5],[-27.45,58.04],[-28.57,52.43],[-28.8,52.2],[-30.6,51.76]]]},\"properties\":{\"id\":3,\"height\":-55,\"biome\":0,\"type\":\"ocean\",\"population\":0,\"state\":0,\"province\":0,\"culture\":0,\"religion\":0,\"neighbors\":[30,13,2,4,14]}},{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[-19.91,57.36],[-22.61,52.32],[-26.33,51.76],[-28.57,52.43],[-27.45,58.04],[-24.52,59.94],[-19.91,57.36]]]},\"properties\":{\"id\":4,\"height\":-88,\"biome\":0,\"type\":\"ocean\",\"population\":0,\"state\":0,\"province\":0,\"culture\":0,\"religion\":0,\"neighbors\":[16,15,14,3]}},{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[-55.12,47.94],[-65.7,49.29],[-67.5,52.99],[-63.79,56.02],[-56.92,56.35],[-54.9,48.17],[-55.12,47.94]]]},\"properties\":{\"id\":5,\"height\":-88,\"biome\":0,\"type\":\"ocean\",\"population\":0,\"state\":0,\"province\":0,\"culture\":0,\"religion\":0,\"neighbors\":[19,18,17,6]}},{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[-52.76,48.5],[-54.9,48.17],[-56.92,56.35],[-56.25,56.8],[-50.62,52.65],[-50.4,51.64],[-52.76,48.5]]]},\"properties\":{\"id\":6,\"height\":-70,\"biome\":0,\"type\":\"ocean\",\"population\":0,\"state\":0,\"province\":0,\"culture\":0,\"religion\":0,\"neighbors\":[8,19,5,0,7]}},{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[-47.25,49.85],[-50.4,51.64],[-50.62,52.65],[-44.66,57.25],[-44.21,54.22],[-47.25,49.85]]]},\"properties\":{\"id\":7,\"height\":-55,\"biome\":0,\"type\":\"ocean\",\"population\":0,\"state\":0,\"province\":0,\"culture\":0,\"religion\":0,\"neighbors\":[9,8,6,0,1]}},{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[-48.26,47.38],[-50.06,46.93],[-52.76,48.5],[-50.4,51.64],[-47.25,49.85],[-47.14,48.84],[-48.26,47.38]]]},\"properties\":{\"id\":8,\"height\":-70,\"biome\":0,\"type\":\"ocean\",\"population\":0,\"state\":0,\"province\":0,\"culture\":0,\"religion\":0,\"neighbors\":[22,21,19,6,7,9]}},{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[-45.56,48.17],[-47.14,48.84],[-47.25,49.85],[-44.21,54.22],[-42.53,52.2],[-43.42,48.62],[-45.56,48.17]]]},\"properties\":{\"id\":9,\"height\":-9,\"biome\":0,\"type\":\"ocean\",\"population\":0,\"state\":0,\"province\":0,\"culture\":0,\"religion\":0,\"neighbors\":[24,22,8,7,1,10]}},{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[-40.16,51.19],[-41.17,47.27],[-43.42,48.62],[-42.53,52.2],[-40.16,51.19]]]},\"properties\":{\"id\":10,\"height\":-9,\"biome\":0,\"type\":\"ocean\",\"population\":0,\"state\":0,\"province\":0,\"culture\":0,\"religion\":0,\"neighbors\":[1,11,24,9]}}]}";
 	//Copy
 	/*
@@ -47,14 +47,14 @@ int main() {
 	struct cell_info {
 		std::vector<  std::tuple<short, short>  > verticies;
 		int id;
-		int height;
-		int biome;
+		short height;
+		short biome;
 		std::string type;
 		int pop;
-		int country;
-		int sub_country;
-		int culture;
-		int religion;
+		short country;
+		short sub_country;
+		short culture;
+		short religion;
 		std::vector<short> neighbors;
 		void add_coord(short x_coord, short y_coord) {
 			this->verticies.push_back(std::make_tuple(x_coord, y_coord));
@@ -176,7 +176,7 @@ t?B:Username!Username@Username.tcc.domain.com Status: visible
 		}
 		std::cout << dir_azgaar << " as directory exists" << std::endl;
 
-		std::string wanted_file = dir_azgaar + "/" + cell_file_t;
+		std::string wanted_file = dir_azgaar + "/" + cell_file;
 
 		fileStream.open(wanted_file, std::ios::in);	// read contents (don't want to output to this file)
 		std::cout << wanted_file << " opened" << std::endl;
@@ -240,6 +240,37 @@ t?B:Username!Username@Username.tcc.domain.com Status: visible
 
 			// READ FROM CELL_MAP or string, EXTRACT VERTEX DATA, ID DATA, and so on
 			//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+			example_data = file_info;
+
+
+
+
+
+
+
+
+
+			// big error: when using file_info copied into example_data, regex_iterator not dereferenceable. this happens at cell id 345
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 			long cell_counter = -1;
 			//all_cells.push_back(cell_info()); // creates a new element
 					// Divy up example_data into various matches
@@ -304,7 +335,7 @@ t?B:Username!Username@Username.tcc.domain.com Status: visible
 					//std::cout << "Type str: " << std::endl;
 					all_cells[cell_counter].type = (CellData_matches[4].str());
 					//std::cout << "Population: " << std::endl;
-					all_cells[cell_counter].pop = StringToShort(CellData_matches[5].str());
+					all_cells[cell_counter].pop = std::stoi(CellData_matches[5].str());
 					//std::cout << "Country: " << std::endl;
 					all_cells[cell_counter].country = StringToShort(CellData_matches[6].str());
 					//std::cout << "Sub-Country: " << std::endl;
