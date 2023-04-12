b4e90f6f3dfe25d357024131ba612243d3d41ac5
UVENAM <PRO8061@pennwest.edu>
2023-04-12 00:04:15 -0400

    updated name

--- corazon.cpp
+++ corazon.cpp
@@ -562,7 +562,7 @@ VUCO( "", VERSION_STAMP );
 	VUCO ( "FLAGS", "Required settings: HUGE gallery, NO SIMPLE shield, white border of thickness 0, scale 1.333", TRUE );
 	VUCO ( "FLAGS", "Export as PNG, place within flags folder. You should do this 3 or 4 times, and thus will have 3 or 4 .png files", TRUE );
 	VUCO ( "FLAGS", "You MUST state the number of flags horizontally in the file name, underscore, and then a unique identifier after. EX: 5_A.png OR 20_1Julius.png", TRUE ); // Vertical separation is 8 pixels
-	VUCO ( "FLAGS", "Regions will be using flags from the same pack/file", TRUE );
+	VUCO ( "FLAGS", "Regions will be using flags from the same pack/file", TRUE ); 
 	VUCO ( "FLAGS", "CUSTOM flags can be done AFTER mass generation, it is up to YOU to edit this", TRUE );
 
 // EU4 needs 128x128 TGA files with TAG.tga format
